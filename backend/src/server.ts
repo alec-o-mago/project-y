5ecb16f805a9a9ffbe8d3a3773548860fec0350e
Alec Santos <alecsantos96@gmail.com>
2024-09-05 11:32:55 -0300

    feat: Hello World on '/' route for testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,10 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('<h1> Server is Running! </h1>');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
