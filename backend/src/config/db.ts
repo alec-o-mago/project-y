1976469135756077728a35fb1654a287f5b7ce7e
Alec Santos <alecsantos96@gmail.com>
2024-09-05 13:58:27 -0300

    fix: Vercel pg error

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 const sequelize = new Sequelize(
     process.env.POSTGRES_URL ?? 'postgres://postgres:postgres@$localhost:5432/postgres',
-    {dialect: 'postgres',}
+    {
+        dialect: 'postgres',
+        dialectModule: pg,
+    }
 );
 
 export default sequelize;
